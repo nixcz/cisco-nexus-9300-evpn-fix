82edcaed1f2edad8a102be07f341edaa974a7105
Radek Šenfeld <rush@logic.cz>
2022-08-01 14:13:27 +0200

    oprava a vylepseni

--- n9kl2routeclear.py
+++ n9kl2routeclear.py
@@ -1,12 +1,24 @@
 # Python3@Nexus
 
+#
+# Copyright (C) 2022 NIX.CZ, z.s.p.o.
+# Author: Radek Senfeld (radek.senfeld@nix.cz)
+# Co-Author: Tomas Hlavacek (tomas.hlavacek@nix.cz)
+#
+# Cisco EEM configuration is as follows:
+#
+#  event manager applet macconflict
+#    event syslog pattern "%L2RIB-2-L2RIB_LOCAL_LEARNT_MAC_PRESENT_AS_REMOTE_STATIC"
+#    action 1 cli python3 bootflash:///n9kl2routeclear.py $_syslog_msg
+#
+
 import re
 import sys
 import cisco
 import syslog
 
-# 2022 Aug  1 13:18:51 LAB2-S1 %L2RIB-2-L2RIB_LOCAL_LEARNT_MAC_PRESENT_AS_REMOTE_STATIC: Locally learnt MAC 00e0.4c3d.269f in topology: 999 already present as remote static
-re_conflict = re.compile(r"(?P<timestamp>[0-9]+\s+[A-Za-z]+\s+[0-9]+\s+[0-9:\.]+)\s+[^\s]+\s+%L2RIB-2-L2RIB_LOCAL_LEARNT_MAC_PRESENT_AS_REMOTE_STATIC: Locally learnt MAC\s+(?P<mac>[0-9a-f\.]+)\s+in topology:\s+(?P<vlan>[0-9]+)\s+already present as remote static.*")
+# %L2RIB-2-L2RIB_LOCAL_LEARNT_MAC_PRESENT_AS_REMOTE_STATIC: Locally learnt MAC 00e0.4c3d.269f in topology: 999 already present as remote static
+re_conflict = re.compile(r"%L2RIB-2-L2RIB_LOCAL_LEARNT_MAC_PRESENT_AS_REMOTE_STATIC: Locally learnt MAC\s+(?P<mac>[0-9a-f\.]+)\s+in topology:\s+(?P<vlan>[0-9]+)\s+already present as remote static")
 
 #
 def log(msg):
@@ -19,13 +31,14 @@ if __name__ == "__main__":
 
 		m = re_conflict.match(msg.strip())
 		if m:
-			# timestamp = m.group("timestamp")
 			mac = m.group("mac")
 			vlan = m.group("vlan")
 
 			log(f"Clearing MAC {mac} for VLAN {vlan}")
 			res = cisco.nxos_cli.nxcli(f"clear mac address-table dynamic address {mac} vlan {vlan}")
 			log(f"CLI call result: {res}")
+		else:
+			log("No match found")
 	except IndexError:
-		log("No syslog message received.. :-(")
+		log("No syslog message received..")
 		sys.exit(1)
